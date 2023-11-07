6acfe690d9cd81809a8bc4884e469e1f5be705bc
ex404aditya <ex404ug@gmail.com>
2023-11-07 19:06:07 +0530

    committing task-1

--- client3.py
+++ client3.py
@@ -35,7 +35,7 @@ def getDataPoint(quote):
     stock = quote['stock']
     bid_price = float(quote['top_bid']['price'])
     ask_price = float(quote['top_ask']['price'])
-    price = (bid_price + ask_price)/2
+    price =(bid_price + ask_price)/2
     return stock, bid_price, ask_price, price
 
 
@@ -45,7 +45,6 @@ def getRatio(price_a, price_b):
     if (price_b == 0):
         return
     return price_a/ price_b
-
 
 # Main
 if __name__ == "__main__":
